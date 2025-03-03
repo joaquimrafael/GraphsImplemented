c74e647b709f227099cc4cf4845bf6e7cd4e6f54
Joaquim Prieto <joaquim.mpp@outlook.com>
2025-03-03 19:27:59 -0300

    reorganize structure

--- main.py
+++ main.py
@@ -3,12 +3,13 @@
 # Antonio Carlos Sciamarelli Neto 10409160
 # Henrique Arabe Neres de Farias 10410152
 
-import filaCircular
-import grafoLista
-import grafoMatriz
-import pilha
+import classes.filaCircular as fc
+import classes.grafoLista as gl
+import classes.grafoMatriz as gm
+import classes.pilha as p
 
 def main():
     print("Hello World")
-    
-__main__ = main()
+
+if __name__ == '__main__':
+    main()
