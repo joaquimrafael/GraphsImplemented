8ef952ef574bd5f4e6260b0b309f51c9c0496941
Henrique <riqueneres01020304@gmail.com>
2025-03-07 00:25:37 -0300

    Exs 7, 8, 9

--- main.py
+++ main.py
@@ -55,6 +55,19 @@ class TGrafoMatrizD(gm.Grafo):
                     return 0
         return 1
     
+    # EX07
+    @staticmethod
+    def readGraphFromFile(filename: str):
+        with open(filename, 'r') as file:
+            lines = [line.strip() for line in file if line.strip()]
+        V = int(lines[0])
+        A = int(lines[1])
+        matriz = [[0 for _ in range(V)] for _ in range(V)]
+        for line in lines[2:]:
+            v, w = map(int,line.split())
+            matriz[v][w] = 1
+        return matriz
+
     # EX13
     def isComplete(self):
         for i in range(self.n):
@@ -77,7 +90,31 @@ class TGrafoMatrizD(gm.Grafo):
 
 # Extensão da classe de Grafo Nao Direcionado em Matriz de Adjacencia 
 class TGrafoMatrizND(gm.Grafo):
+
+    # EX08
+    def insereA(self, v, w):
+        if self.adj[v][w] == 0:
+            self.adj[v][w] = 1
+            self.adj[w][v] = 1
+            self.m += 1
     
+    def removeA(self, v, w):
+        if self.adj[v][w] == 1:
+            self.adj[v][w] = 0
+            self.adj[w][v] = 0
+            self.m -= 1
+    
+    def show(self):
+        print(f"n: {self.n} m: {self.m}")
+        for i in range(self.n):
+            for j in range(self.n):
+                print(f"{self.adj[i][j]:2d}", end=" ")
+            print()
+
+    # EX09
+    def degree(self, v: int) -> int:
+        return sum(self.adj[v])
+
     # EX15
     def isConnected(self):
         visited = [0]* self.n
@@ -93,6 +130,8 @@ class TGrafoMatrizND(gm.Grafo):
                     visited[j] = 1
         
         return int(all(visited))
+
+    
 
 # Extensão da classe de Grafo Direcionado em Lista de Adjacencia
 class TGrafoListaD(gm.Grafo):
@@ -182,7 +221,45 @@ def main():
     gm_nd2.show()
     
     print("\nO grafo 2 é conexo?(1=S/0=N) ->", gm_nd2.isConnected())
+
+    print("Starting main.py")
+
+    filename = "grafo.txt"
+    matriz = TGrafoMatrizD.readGraphFromFile(filename)
+
+    print("Matriz de Adjacência: ")
+    for linha in matriz:
+        print(linha)
+
+    print("\nTeste: Grafo Não Direcionado (Matriz de Adjacência)")
+    gm_nd = TGrafoMatrizND(5)
+    gm_nd.insereA(0,1)
+    gm_nd.insereA(0,2)
+    gm_nd.insereA(1,2)
+    gm_nd.insereA(3,4)
     
+    print("Grafo após inserção das arestas:")
+    gm_nd.show()
+
+    gm_nd.removeA(0,2)
+
+    print("\nGrafo após remoção das arestas entre 0 e 2:")
+    gm_nd.show()
+
+    print("\nTeste: Grau de vértice em Grafo Não Direcionado (Matriz)")
+
+    gm_nd = TGrafoMatrizND(5)
+
+    gm_nd.insereA(0,1)
+    gm_nd.insereA(0,2)
+    gm_nd.insereA(1,2)
+    gm_nd.insereA(3,4)
+
+    gm_nd.show()
+
+    for v in range(gm_nd.n):
+        print(f"Vértice {v}: grau = {gm_nd.degree(v)}")
+
     # print("\nTeste: TGrafoListaDirecionado")
     # gl_dir = TGrafoListaD(5)
     # print(gl_dir)
